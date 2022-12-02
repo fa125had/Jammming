ee8557a3a669345db1d9973bbdd3ef7dfc8fbaab
farshad <far5had@outlook.com>
2022-12-02 15:23:24 +0100

    fix typos

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -44,7 +44,7 @@ const Spotify = {
                 }));
             });
     },
-    savePlatlist(name, trackUris) {
+    savePlaylist(name, trackUris) {
         if (!name || !trackUris.length) {
             return;
         }
@@ -62,8 +62,8 @@ const Spotify = {
                 body: JSON.stringify({ name: name })
             }).then(response => response.json()
             ).then(jsonResponse => {
-                const playlsitId = jsonResponse.id;
-                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlsitId}/tracks`, {
+                const playlistId = jsonResponse.id;
+                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
                     headers: headers,
                     method: 'POST',
                     body: JSON.stringify({ uris: trackUris })
